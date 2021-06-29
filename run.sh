aeea9c8b478d32480423d2c1f8f253ccb1ac1a89
user <user>
2021-06-29 22:22:42 +0800

    updated

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 cd /home/user/git/ircbot_greenbich/
 
+echo pwd
+pwd
+
 export PYTHONHOME=/home/user/vcs/greenbich-runtime/venv3_2/lib/python3.6
 
-./venv3_2/bin/python launch_all.py
+CL=./venv3_2/bin/python launch_all.py
+echo "launching $CL"
+$CL
 
