50cf5dcc4764c6371d3abf1da5d3ee16bb252500
user <user>
2021-10-26 23:39:22 +0800

    fixed near,xmr

--- main.py
+++ main.py
@@ -1420,15 +1420,16 @@ class MyBot:
                                         try:
                                             print('!курс session.get url=' + url)
                                             response = session.get(url, params=parameters)
+                                            if LOG_TRACE: print("cmc.text:", response.text)
                                             cmc = json.loads(response.text)
-                                            if LOG_TRACE: print("cmc:", cmc)
+                                            if LOG_TRACE: print(f"cmc.json: {get_pretty_json_string(cmc)}")
                                             btc_usd = cmc["data"]["BTC"]["quote"]["USD"]["price"]
                                             eth_usd = cmc["data"]["ETH"]["quote"]["USD"]["price"]
                                             dash_usd = cmc["data"]["DASH"]["quote"]["USD"]["price"]
                                             doge_usd = cmc["data"]["DOGE"]["quote"]["USD"]["price"]
                                             zec_usd = cmc["data"]["ZEC"]["quote"]["USD"]["price"]
-                                            near_usd = cmc["data"]["ZEC"]["quote"]["USD"]["price"]
-                                            xmr_usd = cmc["data"]["ZEC"]["quote"]["USD"]["price"]
+                                            near_usd = cmc["data"]["NEAR"]["quote"]["USD"]["price"]
+                                            xmr_usd = cmc["data"]["XMR"]["quote"]["USD"]["price"]
                                             btc_usd_str = str(self.format_currency(btc_usd))
                                             eth_usd_str = str(self.format_currency(eth_usd))
                                             dash_usd_str = str(self.format_currency(dash_usd))
@@ -1437,7 +1438,7 @@ class MyBot:
                                             near_usd_str = str(self.format_currency(near_usd))
                                             xmr_usd_str = str(self.format_currency(xmr_usd))
 
-                                            rate_cmc_str += f'\x02BTC/USD:\x02 {btc_usd_str} \x02ETH/USD:\x02 {eth_usd_str} \x02NEAR/USD:\x02 {near_usd_str} \x02DASH/USD:\x02 {dash_usd_str} \x02ZEC/USD:\x02 {zec_usd_str} \x02XMR/USD:\x02 {XMR_usd_str}'  # \x02DOGE/USD:\x02 {doge_usd_str}
+                                            rate_cmc_str += f'\x02BTC/USD:\x02 {btc_usd_str} \x02ETH/USD:\x02 {eth_usd_str} \x02NEAR/USD:\x02 {near_usd_str} \x02DASH/USD:\x02 {dash_usd_str} \x02ZEC/USD:\x02 {zec_usd_str} \x02XMR/USD:\x02 {xmr_usd_str}'  # \x02DOGE/USD:\x02 {doge_usd_str}
 
                                         except (ConnectionError, Timeout, TooManyRedirects) as e:
                                             traceback.print_exc()
